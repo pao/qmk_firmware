092403c0170d24b856cd10c97667fb1bcd4e31fd
Patrick O'Leary <patrick.oleary@gmail.com>
2018-02-03 21:31:35 -0600

    tap-dance insert on delete key

--- keyboards/xd75/keymaps/planck+pad/keymap.c
+++ keyboards/xd75/keymaps/planck+pad/keymap.c
@@ -27,15 +27,18 @@ enum {
 
 enum {
     TD_ENTER_NUMLOCK = 0,
+    TD_DEL_INS,
 };
 
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_ENTER_NUMLOCK] = ACTION_TAP_DANCE_DOUBLE(KC_KP_ENTER, KC_NUMLOCK),
+    [TD_DEL_INS] = ACTION_TAP_DANCE_DOUBLE(KC_DEL, KC_INS)
 };
 
 // custom keycodes
 #define Kc_CPFN LT(LAYER_FN, KC_CAPSLOCK)
 #define Kc_NMFN LT(LAYER_NUMFN, TD(TD_ENTER_NUMLOCK))
+#define Kc_DLNS TD(TD_DEL_INS)
 #define Kc_SFSL MT(MOD_RSFT, KC_SLASH)
 #define Kc_SFBS MT(MOD_RSFT, KC_BSLASH)
 #define Kc_LOWR KC_NO
@@ -49,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSPC ,KC_P7   ,KC_P8   ,KC_P9  },
         {Kc_CPFN ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,KC_P4   ,KC_P5   ,KC_P6  },
         {KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,Kc_SFSL ,Kc_SFBS ,KC_P1   ,KC_P2   ,KC_P3  },
-        {KC_GRV  ,KC_LGUI ,KC_LCTL ,KC_LALT ,Kc_LOWR ,KC_ENT  ,KC_SPC  ,Kc_RASE ,KC_LALT ,KC_RCTL ,KC_LBRC ,KC_DEL  ,KC_P0   ,Kc_NMFN ,KC_PDOT},
+        {KC_GRV  ,KC_LGUI ,KC_LCTL ,KC_LALT ,Kc_LOWR ,KC_ENT  ,KC_SPC  ,Kc_RASE ,KC_LALT ,KC_RCTL ,KC_LBRC ,Kc_DLNS ,KC_P0   ,Kc_NMFN ,KC_PDOT},
     },
 
     [LAYER_FN] = {
