82858176c52427f79576be1e3cbfb65d37e0f608
Patrick O'Leary <patrick.oleary@gmail.com>
2018-02-03 21:22:13 -0600

    dup rshift onto both slashes; make both alts left (PuTTY doens't grok ralt)

--- keyboards/xd75/keymaps/planck+pad/keymap.c
+++ keyboards/xd75/keymaps/planck+pad/keymap.c
@@ -37,6 +37,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define Kc_CPFN LT(LAYER_FN, KC_CAPSLOCK)
 #define Kc_NMFN LT(LAYER_NUMFN, TD(TD_ENTER_NUMLOCK))
 #define Kc_SFSL MT(MOD_RSFT, KC_SLASH)
+#define Kc_SFBS MT(MOD_RSFT, KC_BSLASH)
 #define Kc_LOWR KC_NO
 #define Kc_RASE MO(LAYER_NAV)
 #define Kc_TSK  LCTL(S(KC_ESC)) // Windows Task Manager
@@ -47,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         {KC_ESC  ,KC_1    ,KC_2    ,KC_3    ,KC_4    ,KC_5    ,KC_6    ,KC_7    ,KC_8    ,KC_9    ,KC_0    ,KC_MINS ,KC_EQL  ,KC_PAST ,KC_PMNS},
         {KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,KC_BSPC ,KC_P7   ,KC_P8   ,KC_P9  },
         {Kc_CPFN ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,KC_QUOT ,KC_P4   ,KC_P5   ,KC_P6  },
-        {KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,Kc_SFSL ,KC_BSLS ,KC_P1   ,KC_P2   ,KC_P3  },
-        {KC_GRV  ,KC_LGUI ,KC_LCTL ,KC_LALT ,Kc_LOWR ,KC_ENT  ,KC_SPC  ,Kc_RASE ,KC_RALT ,KC_RCTL ,KC_LBRC ,KC_DEL  ,KC_P0   ,Kc_NMFN ,KC_PDOT},
+        {KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,Kc_SFSL ,Kc_SFBS ,KC_P1   ,KC_P2   ,KC_P3  },
+        {KC_GRV  ,KC_LGUI ,KC_LCTL ,KC_LALT ,Kc_LOWR ,KC_ENT  ,KC_SPC  ,Kc_RASE ,KC_LALT ,KC_RCTL ,KC_LBRC ,KC_DEL  ,KC_P0   ,Kc_NMFN ,KC_PDOT},
     },
 
     [LAYER_FN] = {
